840849ea1f0c2c17b6c9070860411bad42143fa4
hadinhtu97 <hadinhtu97@gmail.com>
2020-12-09 16:36:18 +0700

    create schema and model

--- app.js
+++ app.js
@@ -14,6 +14,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use(express.static(__dirname + "/public"));
 
+const dbErr = { error: "Some error happened, please try angain later" };
+const userSchema = new mongoose.Schema({
+    username: String
+})
+const User = mongoose.model('User', userSchema);
+const exerciseSchema = new mongoose.Schema({
+    userId: String,
+    description: String,
+    duration: Number,
+    date: Date
+})
+const Exercise = mongoose.model('Excerise', exerciseSchema);
+
+
+
+
+
+
+
+
+
 app.listen(port, () => {
     console.log('Your app is listening on port ' + port);
 })
