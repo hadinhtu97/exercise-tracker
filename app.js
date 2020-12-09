abb970199ba4715d8c3a925148fb4bdb4dca05bd
hadinhtu97 <hadinhtu97@gmail.com>
2020-12-09 16:08:09 +0700

    install mongodb, mongoose package, connect to db

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
